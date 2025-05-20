d3457ca7a1a2b69f397998c508239ca99a994a76
Ronney Rocha <ronneyrv@gmail.com>
2025-05-20 13:19:20 -0300

    feature: add API pedabete

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,27 @@
+import { Card } from "@mui/material";
+import Ask from "../../components/Asks";
+import { useEffect, useState } from "react";
+
 export default function Home() {
-    return(
-        <h1>Pagina home2</h1>
-    )
+  const [faqs, setFaqs] = useState([]);
+
+  useEffect(() => {
+    let url = "http://admin.pedabete.app.br/api/faq";
+    fetch(url)
+      .then((res) => res.json())
+      .then((dados) => setFaqs(dados));
+  }, []);
+  return (
+    <div>
+      {console.log(faqs)}
+      <Card sx={{ margin: 5 }}>
+        <h1>Lista FAQ</h1>
+        {faqs.map((item) => (
+  <div key={item.id}>
+    <Ask question={item.question} answer={item.answer} />
+  </div>
+))}
+      </Card>
+    </div>
+  );
 }
