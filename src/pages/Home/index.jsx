57b404817d223ca5c7193f01ecfd0d629d155b20
Ronney Rocha <ronneyrv@gmail.com>
2025-05-21 18:12:07 -0300

    feature: styling Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
-import { Card } from "@mui/material";
+import { Box, Pagination, Stack, Typography } from "@mui/material";
 import Ask from "../../components/Asks";
 import { useEffect, useState } from "react";
 
 export default function Home() {
   const [faqs, setFaqs] = useState([]);
+  const [page, setPage] = useState(1);
+  const itemsPerPage = 5;
 
   useEffect(() => {
     let url = "http://admin.pedabete.app.br/api/faq";
@@ -11,17 +13,49 @@ export default function Home() {
       .then((res) => res.json())
       .then((dados) => setFaqs(dados));
   }, []);
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
+  const startIndex = (page - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentItems = faqs.slice(startIndex, endIndex);
+  const totalPages = Math.ceil(faqs.length / itemsPerPage);
+
   return (
-    <div>
-      {console.log(faqs)}
-      <Card sx={{ margin: 5 }}>
-        <h1>Lista FAQ</h1>
-        {faqs.map((item) => (
-  <div key={item.id}>
-    <Ask question={item.question} answer={item.answer} />
-  </div>
-))}
-      </Card>
-    </div>
+    <Box
+      sx={{
+        margin: "40px auto",
+        maxWidth: 800,
+        padding: 3,
+        backgroundColor: "#f9f9f9",
+        borderRadius: 2,
+        boxShadow: 3,
+      }}
+    >
+      <Typography
+        variant="h4"
+        component="h1"
+        gutterBottom
+        sx={{ fontWeight: "bold", textAlign: "center" }}
+      >
+        Lista de Perguntas Frequentes (FAQ)
+      </Typography>
+      <Stack spacing={2}>
+        {currentItems.map((item) => (
+          <div key={item.id}>
+            <Ask question={item.question} answer={item.answer} />
+          </div>
+        ))}
+        <Box sx={{ display: "flex", justifyContent: "center", mt: 3 }}>
+          <Pagination
+            count={totalPages}
+            page={page}
+            onChange={handleChange}
+            shape="rounded"
+            color="primary"
+          />
+        </Box>
+      </Stack>
+    </Box>
   );
 }
